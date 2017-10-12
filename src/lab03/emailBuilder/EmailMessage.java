4a1f2fa94ea08648b5e957ef6853db2692909724
gsxxx <infopreenk@gmail.com>
2017-10-12 13:36:18 +0200

    almost finishing emailBuilder

--- src/lab03/emailBuilder/EmailMessage.java
+++ src/lab03/emailBuilder/EmailMessage.java
@@ -1,6 +1,11 @@
 package lab03.emailBuilder;
 
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class EmailMessage {
 
@@ -12,26 +17,110 @@ public class EmailMessage {
     private LinkedList<String> cc; //optional
     private LinkedList<String> bcc; // optional
 
-    //Przykładowy konstruktor (można założyć, że pola opcjonalne mogą być null)
 
-
-    public EmailMessage(String from, LinkedList<String> to, String subject, String content, String mimeType, LinkedList<String> cc, LinkedList<String> bcc) {
-        this.from = from;
-        this.to = to;
-        if(subject!= null) this.subject = subject;
-        if(content!= null) this.content = content;
-        if(mimeType!= null) this.mimeType = mimeType;
-        if(cc!= null) this.cc = cc;
-        if(bcc!= null) this.bcc = bcc;
+    public EmailMessage(Builder builder) {
+        this.from = builder.from;
+        this.to = builder.to;
+        this.subject = builder.subject;
+        this.content = builder.content;
+        this.mimeType = builder.mimeType;
+        this.cc = builder.cc;
+        this.bcc = builder.bcc;
     }
+
 
     public static Builder builder() {
         return new EmailMessage.Builder();
     }
 
     public static class Builder {
+        private String from; //required (must be e-mail)
+        private LinkedList<String> to; //required at least one (must be e-mail)
+        private String subject; //optional
+        private String content; //optional
+        private String mimeType;  // optional
+        private LinkedList<String> cc; //optional
+        private LinkedList<String> bcc;
+
+        public Builder from(String input) {
+            this.from = input;
+            return this;
+        }
+
+        public Builder to(String... input) {
+            this.to = new LinkedList<>(Arrays.asList(input));
+            return this;
+        }
+
+        public Builder subject(String input) {
+            this.subject = input;
+            return this;
+        }
+
+        public Builder content(String input) {
+            this.content = input;
+            return this;
+        }
+
+        public Builder mimeType(String input) {
+            this.mimeType = input;
+            return this;
+        }
+
+        public Builder cc(String... input) {
+            this.cc = new LinkedList<>(Arrays.asList(input));
+            return this;
+        }
+
+        public Builder bcc(String... input) {
+            this.bcc = new LinkedList<>(Arrays.asList(input));
+            return this;
+        }
+
+        public EmailMessage build() {
+            return new EmailMessage(this);
+        }
     }
 
+    @Override
+    public String toString() {
+        return String.format("%s\n%s\n%s\n%s\n", from, to, subject, content);
+    }
+
+    public void send() {
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.setProperty("mail.smtp.host", "localhost");
+
+        // Get the default Session object.
+        Session session = Session.getDefaultInstance(properties);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to.get(0)));
+
+            // Set Subject: header field
+            message.setSubject(subject);
+
+            // Now set the actual message
+            message.setText(content);
+
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (Exception mex) {
+            mex.printStackTrace();
+        }
+
+    }
 }
 
 
--- src/lab03/emailBuilder/emailBuilder.java
+++ src/lab03/emailBuilder/emailBuilder.java
@@ -1,7 +1,17 @@
 package lab03.emailBuilder;
 
-public class emailBuilder {
-    public static void main(String args[]){
 
+
+public class emailBuilder {
+    public static void main(String args[]) {
+        EmailMessage wiadomosc = EmailMessage.builder()
+                .from("middleOfNowhere@agh.edu.pl")
+                .to("prenc@student.agh.edu.pl")
+                .subject("Mail testowy")
+                .content("Brak tresci")
+                .build();
+
+        System.out.println(wiadomosc.toString());
+        wiadomosc.send();
     }
 }
