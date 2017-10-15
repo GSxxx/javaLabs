8089165aa09ca968c9c6d1ccb96dea014b892d4e
vascodagama123 <vascodagama@mailinator.com>
2017-10-15 15:47:42 +0200

    small changes in crossword

--- src/lab03/crosswordUpgraded/BoardCell.java
+++ src/lab03/crosswordUpgraded/BoardCell.java
@@ -2,13 +2,26 @@ package lab03.crosswordUpgraded;
 
 public class BoardCell {
 
-
+    private String letter;
 
     public void setContent(String content) {
-
+        this.letter = content;
     }
 
     public String getContent() {
-        return "";
+        return letter;
     }
+
+    public void disableHorizStart(){};
+    public void disableVertStart(){};
+    public void disableHorizInner(){};
+    public void disableVertInner(){};
+    public void disableHorizEnd(){};
+    public void disableVertEnd(){};
+    public void enableHorizStart(){};
+    public void enableVertStart(){};
+    public void enableHorizInner(){};
+    public void enableVertInner(){};
+    public void enableHorizEnd(){};
+    public void enableVertEnd(){};
 }
